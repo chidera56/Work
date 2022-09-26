1a7eb6f64ad22f3934abfdb1363cf17ee71d9472
chidera56 <114371649+chidera56@users.noreply.github.com>
2022-09-25 21:36:27 -0400

    Add files via upload

---
+++ Area&CircumferenceOfACircle/src/Area_Circumference/Area_Circumference.java
@@ -0,0 +1,41 @@
+package Area_Circumference;
+
+/**
+ * 
+ * @author Chidera
+ */
+
+import java.util.Scanner;
+
+public class Area_Circumference {
+    public static String calculateCircumference;
+
+    public static String calculateArea;
+
+    int radius;
+
+    Scanner sc = new Scanner(System.in);
+
+    Area_Circumference(int radius){
+        this.radius = radius;
+    }
+    
+    public double calculateArea(){
+        double area = 3.14 * (radius * radius);
+        return area;
+    }
+
+    public double calculateCircumference(){
+        double circumference = 2 *(3.14 * radius);
+        return circumference;
+    }
+
+    int getRadius(){
+        return radius;
+    }
+
+    void setRadius(int radius){
+        this.radius = radius;
+    } 
+
+}
