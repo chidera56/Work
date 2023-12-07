e4f239aa79cf229b80e89bece99b55d2a00fbeba
chidorie56 <114371649+chidorie56@users.noreply.github.com>
2023-12-06 22:39:53 -0400

    Create mad_libs_game.py

---
+++ Python Projects/mad_libs_game.py
@@ -0,0 +1,11 @@
+adjective1 = input("Enter an adjective: ")
+noun = input("Enter a noun: ")
+adjective2 = input("Enter an adjective: ")
+verb = input("Enter a verb: ")
+adjective3 = input("Enter an adjective: ")
+
+
+print(f"Yesterday, I watched a {adjective1} show")
+print(f"In an amusement park, I saw {noun}")
+print(f"{noun} was {adjective2} and {verb}ing")
+print(f"I was {adjective3}")
