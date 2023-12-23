f15ef89ebb27ff65e12de4f5cf3ba5aad5057683
chidorie56 <114371649+chidorie56@users.noreply.github.com>
2023-12-22 21:14:53 -0400

    updates

---
+++ Java Projects/ATM/src/Menu.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+import java.text.DecimalFormat;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Menu extends Account {
+    public static void main(String[] args) throws Exception {
+        Scanner input = new Scanner(System.in);
+        DecimalFormat moneyformat = new DecimalFormat("'$'###,##0.00");
+
+        HashMap<Integer, Integer> data = new Hashmap<Integer, Integer>();
+
+    }
+}
