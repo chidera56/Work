d651a17cc615616a5d3952001fa8083207bbb2cd
chidera56 <chidera.ezenwaka@yahoo.com>
2024-04-30 11:18:34 -0400

    updates

--- React Projects/mywebsite/src/App.js
+++ React Projects/mywebsite/src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>Yo</h1>
+      <p>Hi, My name is Chidera Ezenwaka and I aspire to be a software engineer/technician.</p>
     </div>
   );
 }
